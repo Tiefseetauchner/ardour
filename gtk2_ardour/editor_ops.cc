3302c1ecf0aa52e280631ecde6966eafd6eda083
Paul Davis <paul@linuxaudiosystems.com>
2020-11-27 15:44:34 -0700

    fix up memento command use for tempo map undo/redo

--- gtk2_ardour/editor_ops.cc
+++ gtk2_ardour/editor_ops.cc
@@ -7117,8 +7117,7 @@ Editor::define_one_bar (timepos_t const & start, timepos_t const & end)
 
 	XMLNode& after (tmap->get_state());
 
-#warning NUTEMPO memento command tempo map issues
-	// _session->add_command (new MementoCommand<TempoMap>(_session->tempo_map(), &before, &after));
+	_session->add_command (new MementoCommand<Temporal::TempoMap> (new Temporal::TempoMap::MementoBinder(), &before, &after));
 	commit_reversible_command ();
 }
 
@@ -8057,8 +8056,7 @@ Editor::insert_time (
 		XMLNode& before (tmap->get_state());
 		tmap->insert_time (pos, samples);
 		XMLNode& after (tmap->get_state());
-#warning NUTEMPO memento command tempo map issues
-		// _session->add_command (new MementoCommand<TempoMap>(_session->tempo_map(), &before, &after));
+		_session->add_command (new MementoCommand<Temporal::TempoMap> (new Temporal::TempoMap::MementoBinder(), &before, &after));
 	}
 
 	if (in_command) {
@@ -8238,8 +8236,7 @@ Editor::remove_time (timepos_t const & pos, timecnt_t const & duration, InsertTi
 				in_command = true;
 			}
 			XMLNode& after (tmap->get_state());
-#warning NUTEMPO memento command tempo map issue
-			//_session->add_command (new MementoCommand<TempoMap>(_session->tempo_map(), &before, &after));
+			_session->add_command (new MementoCommand<Temporal::TempoMap> (new Temporal::TempoMap::MementoBinder(), &before, &after));
 		}
 	}
 
